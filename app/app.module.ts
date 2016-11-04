6cadfc3e81395e71ba9145b476482bddc16431fd
Vinicius Bandeira <Vinicius@MacBook-Air-de-Vinicius.local>
2016-11-04 19:58:13 -0200

    Search with errors yet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,28 +1,38 @@
+import './rxjs-extensions';
+
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
 
+import { AppRoutingModule }     from './app-routing.module';
+
+//Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-details.component';
+import { HeroSearchComponent }  from './hero-search.component';
 import { HeroService }          from './hero.service';
-
-import { AppRoutingModule }     from './app-routing.module';
 
 
 @NgModule({
     imports: [ 
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     declarations: [ 
         AppComponent,
         DashboardComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
+        HeroSearchComponent
     ],
     providers: [ HeroService ],
     bootstrap: [ AppComponent ],
